465ef1c6cb13ffe747f9414325e86e851cc7f82a
KhaledHAnter <khaldntr11@gmail.com>
2026-07-22 22:13:17 +0300

    feat: update AppLoadingIndicator to use LoadingAnimationWidget and adjust dependencies

--- lib/widgets/app_loading_indicator.dart
+++ lib/widgets/app_loading_indicator.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 import '../core/helpers/app_colors.dart';
+import '../core/helpers/dimensions.dart';
 
 class AppLoadingIndicator extends StatelessWidget {
   const AppLoadingIndicator({
     super.key,
-    this.size = 24,
+    this.size,
     this.color = AppColors.primary,
-    this.strokeWidth = 2.5,
     this.padding = EdgeInsets.zero,
     this.unconstrained = true,
   });
 
-  final double size;
+  final double? size;
   final Color color;
-  final double strokeWidth;
   final EdgeInsetsGeometry padding;
   final bool unconstrained;
 
@@ -22,13 +22,9 @@ class AppLoadingIndicator extends StatelessWidget {
   Widget build(BuildContext context) {
     final indicator = Padding(
       padding: padding,
-      child: SizedBox(
-        height: size,
-        width: size,
-        child: CircularProgressIndicator(
-          strokeWidth: strokeWidth,
-          valueColor: AlwaysStoppedAnimation<Color>(color),
-        ),
+      child: LoadingAnimationWidget.threeArchedCircle(
+        color: color,
+        size: size ?? 24.radius,
       ),
     );
     if (!unconstrained) return indicator;
